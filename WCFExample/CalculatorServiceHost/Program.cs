ee1ae5077ee34409b77957ad0d7477ddb3d733da
RidgeX <ridge.shrubsall@gmail.com>
2016-12-05 15:30:16 +0800

    Add CalculatorServiceHost

---
+++ WCFExample/CalculatorServiceHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using CalculatorServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService));
+
+            try
+            {
+                selfHost.Open();
+
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
