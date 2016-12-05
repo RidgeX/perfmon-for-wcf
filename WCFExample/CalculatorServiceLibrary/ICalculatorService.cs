34ff6e1e87d7c86f86830b9bb0588e96cde2931a
RidgeX <ridge.shrubsall@gmail.com>
2016-12-05 15:21:36 +0800

    Add CalculatorServiceLibrary

---
+++ WCFExample/CalculatorServiceLibrary/ICalculatorService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorServiceLibrary
+{
+    [ServiceContract(Namespace = "http://Calculator")]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
