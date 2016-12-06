8b264a8ea26a02c0eed70ed4c284ef47464f1408
RidgeX <ridge.shrubsall@gmail.com>
2016-12-06 14:14:32 +0800

    Add CalculatorServiceWindows

---
+++ WCFExample/CalculatorServiceWindows/ProjectInstaller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceWindows
+{
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "CalculatorService";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
---
+++ WCFExample/CalculatorServiceWindows/Service.cs
@@ -0,0 +1,46 @@
+﻿using CalculatorServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceWindows
+{
+    public class Service : ServiceBase
+    {
+        private ServiceHost serviceHost = null;
+
+        public Service()
+        {
+            ServiceName = "CalculatorService";
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new Service());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            serviceHost = new ServiceHost(typeof(CalculatorService));
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+    }
+}
