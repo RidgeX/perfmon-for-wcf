752d27fb4941971ac44b4c3588daa8c8dd2020b6
RidgeX <ridge.shrubsall@gmail.com>
2016-12-09 15:57:48 +0800

    Implement TreeView counter selection

--- Perfmon/Perfmon/MainWindow.xaml.cs
+++ Perfmon/Perfmon/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using LiveCharts.Configurations;
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
@@ -51,7 +52,9 @@ namespace Perfmon
             }
         }
         public DispatcherTimer Timer { get; set; }
+        public ObservableCollection<CategoryItem> Categories { get; set; }
         public List<PerformanceCounter> Counters { get; set; }
+        public Dictionary<PerformanceCounter, LineSeries> CounterMap { get; set; }
 
         public MainWindow()
         {
@@ -73,24 +76,14 @@ namespace Perfmon
             };
             Timer.Tick += Timer_Tick;
 
-            Counters = new List<PerformanceCounter>();
-            //Counters.Add(new PerformanceCounter("ServiceModelOperation 4.0.0.0", "Calls", "Calcu50.ICalculatorService.Add@HTTP:||LOCALHOST:8000|CALCULATOR|"));
-            Counters.Add(new PerformanceCounter("ServiceModelOperation 4.0.0.0", "Calls Per Second", "Calcu50.ICalculatorService.Add@HTTP:||LOCALHOST:8000|CALCULATOR|"));
-            //Counters.Add(new PerformanceCounter("ServiceModelOperation 4.0.0.0", "Calls Duration", "Calcu50.ICalculatorService.Add@HTTP:||LOCALHOST:8000|CALCULATOR|"));
-            //Counters.Add(new PerformanceCounter("ServiceModelOperation 4.0.0.0", "Calls Outstanding", "Calcu50.ICalculatorService.Add@HTTP:||LOCALHOST:8000|CALCULATOR|"));
-            Counters.Add(new PerformanceCounter("Test category", "Test counter"));
+            Categories = new ObservableCollection<CategoryItem>();
+            Categories.Add(MakeCategoryItem("ServiceModelEndpoint 4.0.0.0"));
+            Categories.Add(MakeCategoryItem("ServiceModelOperation 4.0.0.0"));
+            Categories.Add(MakeCategoryItem("ServiceModelService 4.0.0.0"));
+            Categories.Add(MakeCategoryItem("Test category"));
 
-            for (var i = 0; i < Counters.Count; i++)
-            {
-                LineSeries series = new LineSeries()
-                {
-                    PointGeometrySize = 9,
-                    StrokeThickness = 2,
-                    Title = Counters[i].CounterName,
-                    Values = new ChartValues<MeasureModel>()
-                };
-                SeriesCollection.Add(series);
-            }
+            Counters = new List<PerformanceCounter>();
+            CounterMap = new Dictionary<PerformanceCounter, LineSeries>();
 
             DataContext = this;
         }
@@ -102,6 +95,83 @@ namespace Perfmon
             if (PropertyChanged != null)
             {
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private CategoryItem MakeCategoryItem(string categoryName)
+        {
+            var instanceItems = new ObservableCollection<InstanceItem>();
+            var category = PerformanceCounterCategory.GetCategories().First(c => c.CategoryName == categoryName);
+
+            string[] instanceNames = category.GetInstanceNames();
+            Array.Sort(instanceNames);
+
+            if (instanceNames.Any())
+            {
+                foreach (string instanceName in instanceNames)
+                {
+                    if (category.InstanceExists(instanceName))
+                    {
+                        instanceItems.Add(MakeInstanceItem(instanceName, category.GetCounters(instanceName)));
+                    }
+                }
+            }
+            else
+            {
+                instanceItems.Add(MakeInstanceItem("*", category.GetCounters(string.Empty)));
+            }
+
+            return new CategoryItem(categoryName, instanceItems);
+        }
+
+        private InstanceItem MakeInstanceItem(string instanceName, PerformanceCounter[] counters)
+        {
+            var counterItems = new ObservableCollection<CounterItem>();
+
+            foreach (PerformanceCounter counter in counters)
+            {
+                counterItems.Add(MakeCounterItem(counter.CounterName, counter));
+            }
+
+            return new InstanceItem(instanceName, counterItems);
+        }
+
+        private CounterItem MakeCounterItem(string counterName, PerformanceCounter counter)
+        {
+            CounterItem counterItem = new CounterItem(counterName, counter);
+            counterItem.PropertyChanged += CounterItem_PropertyChanged;
+            return counterItem;
+        }
+
+        private void CounterItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("IsChecked"))
+            {
+                var counterItem = (CounterItem) sender;
+
+                if (counterItem.IsChecked == true)
+                {
+                    LineSeries series = new LineSeries()
+                    {
+                        PointGeometrySize = 9,
+                        StrokeThickness = 2,
+                        Title = counterItem.Name,
+                        Values = new ChartValues<MeasureModel>()
+                    };
+                    SeriesCollection.Add(series);
+                    CounterMap.Add(counterItem.Counter, series);
+                    Counters.Add(counterItem.Counter);
+                }
+                else
+                {
+                    LineSeries series;
+                    if (CounterMap.TryGetValue(counterItem.Counter, out series))
+                    {
+                        Counters.Remove(counterItem.Counter);
+                        CounterMap.Remove(counterItem.Counter);
+                        SeriesCollection.Remove(series);
+                    }
+                }
             }
         }
 
@@ -127,19 +197,23 @@ namespace Perfmon
 
         private void Timer_Tick(object sender, EventArgs eventArgs)
         {
-            for (int i = 0; i < Counters.Count; i++)
+            foreach (PerformanceCounter counter in Counters)
             {
-                var now = DateTime.Now;
-                var values = SeriesCollection[i].Values;
-
-                values.Add(new MeasureModel()
+                LineSeries series;
+                if (CounterMap.TryGetValue(counter, out series))
                 {
-                    DateTime = now,
-                    Value = Counters[i].NextValue()
-                });
-                SetAxisLimits(now);
+                    var now = DateTime.Now;
+                    var values = series.Values;
 
-                if (values.Count > 30) values.RemoveAt(0);
+                    values.Add(new MeasureModel()
+                    {
+                        DateTime = now,
+                        Value = counter.NextValue()
+                    });
+                    SetAxisLimits(now);
+
+                    if (values.Count > 30) values.RemoveAt(0);
+                }
             }
         }
     }
