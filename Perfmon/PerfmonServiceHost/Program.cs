48157331f9749fafaf71aa71bd5cedc9456fbf39
RidgeX <ridge.shrubsall@gmail.com>
2016-12-12 16:25:24 +0800

    Add PerfmonServiceHost

---
+++ Perfmon/PerfmonServiceHost/Program.cs
@@ -0,0 +1,47 @@
+﻿using PerfmonServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerfmonServiceHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PerfmonService service = new PerfmonService();
+            ServiceHost selfHost = new ServiceHost(service);
+
+            try
+            {
+                selfHost.Open();
+
+                Random random = new Random();
+                bool running = true;
+
+                while (running)
+                {
+                    service.Notify(new EventData()
+                    {
+                        Time = DateTime.Now,
+                        Host = "localhost",
+                        Path = @"\Test category\Test counter",
+                        Value = random.NextDouble()
+                    });
+                    Thread.Sleep(1000);
+                }
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
