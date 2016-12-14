3748ad501eca9dff5f3a02549865a6e1880d614c
RidgeX <ridge.shrubsall@gmail.com>
2016-12-14 16:50:54 +0800

    Add support for drag and drop

--- Perfmon/PerfmonClient/MainWindow.xaml.cs
+++ Perfmon/PerfmonClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PerfmonClient.Model;
+﻿using LiveCharts.Wpf;
+using PerfmonClient.Model;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -61,6 +62,50 @@ namespace PerfmonClient
         private void quitMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void treeView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = (TreeViewItem) treeView.SelectedItem;
+
+            if (item != null)
+            {
+                item.IsSelected = false;
+            }
+        }
+
+        private Point dragStart;
+
+        private void TreeViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            dragStart = e.GetPosition(null);
+        }
+
+        private void treeView_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            var item = (TreeViewItem) treeView.SelectedItem;
+
+            if (item != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Vector offset = e.GetPosition(null) - dragStart;
+
+                if (Math.Abs(offset.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(offset.Y) > SystemParameters.MinimumVerticalDragDistance)
+                {
+                    DataObject data = new DataObject(typeof(TreeViewItem), item);
+                    DragDrop.DoDragDrop(treeView, data, DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void CartesianChart_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(TreeViewItem)))
+            {
+                var item = (TreeViewItem) e.Data.GetData(typeof(TreeViewItem));
+                var chart = (CartesianChart) sender;
+                Console.WriteLine("Received: {0}", item.Header);
+            }
         }
     }
 }
