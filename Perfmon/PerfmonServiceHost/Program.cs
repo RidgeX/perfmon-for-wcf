3bf0ebe7475287b07c0d81c1eab7d6d4a379223a
RidgeX <ridge.shrubsall@gmail.com>
2016-12-23 15:02:41 +0800

    Move update code into library

--- Perfmon/PerfmonServiceHost/Program.cs
+++ Perfmon/PerfmonServiceHost/Program.cs
@@ -1,7 +1,6 @@
 ﻿using PerfmonServiceLibrary;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -21,37 +20,11 @@ namespace PerfmonServiceHost
             {
                 selfHost.Open();
 
-                PerformanceCounterCategory pcc = new PerformanceCounterCategory("Processor");
-                Dictionary<string, CounterSample> prevSamples = new Dictionary<string, CounterSample>();
                 bool running = true;
 
                 while (running)
                 {
-                    InstanceDataCollectionCollection idcc = pcc.ReadCategory();
-
-                    foreach (InstanceDataCollection idc in idcc.Values)
-                    {
-                        if (!idc.CounterName.Equals("% Processor Time")) continue;
-
-                        foreach (InstanceData id in idc.Values)
-                        {
-                            string path = string.Format(@"\{0}({1})\{2}", pcc.CategoryName, id.InstanceName, idc.CounterName);
-
-                            CounterSample prevSample;
-                            if (!prevSamples.TryGetValue(path, out prevSample))
-                            {
-                                prevSample = CounterSample.Empty;
-                            }
-                            CounterSample sample = id.Sample;
-                            float value = CounterSample.Calculate(prevSample, sample);
-                            prevSamples[path] = sample;
-
-                            DateTime dateTime = DateTime.FromFileTime(sample.TimeStamp100nSec);
-                            EventData e = new EventData() { DateTime = dateTime, Path = path, Value = value };
-                            service.Notify(e);
-                        }
-                    }
-
+                    service.Update();
                     Thread.Sleep(1000);
                 }
 
--- Perfmon/PerfmonServiceLibrary/PerfmonService.cs
+++ Perfmon/PerfmonServiceLibrary/PerfmonService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -13,11 +14,13 @@ namespace PerfmonServiceLibrary
     public class PerfmonService : IPerfmonService
     {
         private static readonly object _lock;
+        private static readonly Dictionary<string, CounterSample> prevSamples;
         private static readonly Dictionary<string, List<IPerfmonCallback>> subscribers;
 
         static PerfmonService()
         {
             _lock = new object();
+            prevSamples = new Dictionary<string, CounterSample>();
             subscribers = new Dictionary<string, List<IPerfmonCallback>>();
         }
 
@@ -72,6 +75,35 @@ namespace PerfmonServiceLibrary
                 if (subscribers.TryGetValue(path, out list))
                 {
                     list.Remove(callback);
+                }
+            }
+        }
+
+        public void Update()
+        {
+            PerformanceCounterCategory pcc = new PerformanceCounterCategory("Processor");
+            InstanceDataCollectionCollection idcc = pcc.ReadCategory();
+
+            foreach (InstanceDataCollection idc in idcc.Values)
+            {
+                if (!idc.CounterName.Equals("% Processor Time")) continue;
+
+                foreach (InstanceData id in idc.Values)
+                {
+                    string path = string.Format(@"\{0}({1})\{2}", pcc.CategoryName, id.InstanceName, idc.CounterName);
+
+                    CounterSample prevSample;
+                    if (!prevSamples.TryGetValue(path, out prevSample))
+                    {
+                        prevSample = CounterSample.Empty;
+                    }
+                    CounterSample sample = id.Sample;
+                    float value = CounterSample.Calculate(prevSample, sample);
+                    prevSamples[path] = sample;
+
+                    DateTime dateTime = DateTime.FromFileTime(sample.TimeStamp100nSec);
+                    EventData e = new EventData() { DateTime = dateTime, Path = path, Value = value };
+                    Notify(e);
                 }
             }
         }
