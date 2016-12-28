235ebea2326eca344a77d4e2f90d61e50c1bdb0b
RidgeX <ridge.shrubsall@gmail.com>
2016-12-28 12:39:44 +0800

    Add message inspectors for debugging

--- Perfmon/PerfmonServiceHost/Program.cs
+++ Perfmon/PerfmonServiceHost/Program.cs
@@ -3,12 +3,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PerfmonServiceHost
 {
+    public class DebugInspector : IClientMessageInspector, IDispatchMessageInspector
+    {
+        // Request from client
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            Console.WriteLine(request);
+            return null;
+        }
+
+        // Reply to client
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            Console.WriteLine(reply);
+        }
+
+        // Call to client
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            Console.WriteLine(request);
+            return null;
+        }
+
+        public void AfterReceiveReply(ref Message reply, object correlationState) { }
+    }
+
+    public class DebugBehavior : IEndpointBehavior
+    {
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime) { }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            DebugInspector debugInspector = new DebugInspector();
+            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(debugInspector);
+            endpointDispatcher.DispatchRuntime.CallbackClientRuntime.ClientMessageInspectors.Add(debugInspector);
+        }
+
+        public void Validate(ServiceEndpoint endpoint) { }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -18,6 +62,11 @@ namespace PerfmonServiceHost
 
             try
             {
+                /*
+                ServiceEndpoint endpoint = selfHost.Description.Endpoints[0];
+                endpoint.EndpointBehaviors.Add(new DebugBehavior());
+                */
+
                 selfHost.Open();
 
                 bool running = true;
