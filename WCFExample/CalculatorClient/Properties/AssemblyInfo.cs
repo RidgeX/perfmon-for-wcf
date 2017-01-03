604fb325897a62dc7c8c19ebb13cc1d54eff8121
RidgeX <ridge.shrubsall@gmail.com>
2017-01-03 09:43:30 +0800

    Update assembly info

--- WCFExample/CalculatorClient/Properties/AssemblyInfo.cs
+++ WCFExample/CalculatorClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CalculatorClient")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
